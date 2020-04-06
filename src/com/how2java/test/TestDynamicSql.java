e6a0c155a3608d17d2d3aeeb2ab1f1b4e8d0032c
Lyy <liuyangyang09@meituan.com>
2020-04-06 18:44:52 +0800

    dynamicSql

--- src/com/how2java/test/TestDynamicSql.java
+++ src/com/how2java/test/TestDynamicSql.java
@@ -1,6 +1,5 @@
 package com.how2java.test;
 
-import com.how2java.pojo.Category;
 import com.how2java.pojo.Product;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -9,11 +8,12 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TestProduct {
+public class TestDynamicSql {
 
     @SuppressWarnings("all")
     public static void main(String[] args) throws IOException {
@@ -25,19 +25,38 @@ public class TestProduct {
         //2、然后再根据sqlSessionFactory 得到session
         SqlSession session = sqlSessionFactory.openSession();
 
-
-        //3、最后通过session的selectList方法，调用sql语句listCategory。listCategory这个就是在配置文件Category.xml中那条sql语句设置的id。
-        //执行完毕之后，得到一个Category集合，遍历即可看到数据。
-        List<Product> products = session.selectList("ManyByOneListProduct");
-
-//        for (Product product : products) {
-//            System.out.println("name:" + product.getName() + " " + "prcie:" + product.getPrice());
+//        System.out.println("查询所有");
+//        List<Product> allProducts = session.selectList("DynamicSqlListProduct");
+//        for (Product product : allProducts) {
+//            System.out.println(product);
 //        }
+//
+        System.out.println("模糊查询");
+        Map<String, String> param = new HashMap<>();
+        param.put("name", "z");
+//        param.put("price","90");
 
-        for (Product p : products) {
-            System.out.println(p + " 对应的分类是 \t " + p.getCategory());
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(3);
+//        list.add(5);
+
+        List<Product> likeProducts = session.selectList("bindDynamicSqlListProduct", param);
+
+        for (Product product : likeProducts) {
+            System.out.println(product);
         }
 
+//        Product p = new Product();
+//        p.setId(6);
+//        p.setName("product zz");
+//        p.setPrice(99.98f);
+//        session.update("TrimDynamicSqlUpdateProduct", p);
+//
+//        listAll(session);
+
+        session.commit();
+        session.close();
     }
 
     private static void listAll(SqlSession session) {
